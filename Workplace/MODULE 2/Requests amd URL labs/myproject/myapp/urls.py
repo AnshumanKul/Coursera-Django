8877c7a2d691ce98fefbdbc388e37743b2cf9358
AnshumanKul <anshumansk25@gmail.com>
2025-03-06 00:04:56 +0530

    Fixing last commit

--- Workplace/MODULE 2/Requests amd URL labs/myproject/myapp/urls.py
+++ Workplace/MODULE 2/Requests amd URL labs/myproject/myapp/urls.py
@@ -1,5 +1,11 @@
-from django.urls import path 
+from django.urls import path
 from . import views
-urlpatterns=[
-    path('drinks/<str:drink_name>', views.drinks, name='drink_name'), 
+
+urlpatterns = [
+    path('', views.home, name="home"),
+]
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('drinks/<str:drink_name>', views.drinks, name="drink_name"), 
 ]
--- Workplace/MODULE 2/Requests amd URL labs/myproject/myapp/views.py
+++ Workplace/MODULE 2/Requests amd URL labs/myproject/myapp/views.py
@@ -1,14 +1,16 @@
-from http.client import HTTPResponse
-from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return HttpResponse("<h1> Welcome to Little Lemon! </h1> ")
 
 def drinks(request, drink_name):
+
     drink = {
-        'mocha' : 'type of coffee',
-        'tea' : 'type of hot beverage',
+        'mocha': 'type of coffee',
+        'tea': 'type of beverage',
         'lemonade': 'type of refreshment'
     }
-    choice_of_drink = drink[drink_name]
-    return HttpResponse(f"<h2>{drink_name}</h2> " + choice_of_drink)
+
+    choice_of_drink = drink.get(drink_name, "Unknown drink")
+
+    return HttpResponse(f"<h2>{drink_name.capitalize()}</h2> {choice_of_drink}")
