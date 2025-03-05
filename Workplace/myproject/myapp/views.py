3e147ca262be1fc55eafbc2c19b8b0063c94f755
AnshumanKul <anshumansk25@gmail.com>
2025-03-05 22:32:12 +0530

    Module 2 Lab 1

--- Workplace/myproject/myapp/views.py
+++ Workplace/myproject/myapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-#views.py
+from django.http import HttpResponse
 # Create your views here.
+def home(request):
+    string1="<h1> Welcome to Little Lemon! </h1>"
+    return HttpResponse(string1)
